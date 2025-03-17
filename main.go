ada465c69e266a77be645c1fc1d1947495d2ae1b
JWSch4fer <joeschafer28@gmail.com>
2025-03-17 13:56:27 -0400

    add function to retrieve html from base url

--- main.go
+++ main.go
@@ -1,8 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
 
+	// map of the available cli commands
+	args := os.Args
+
+	if len(args) == 1 {
+		fmt.Printf("no website provided\n")
+		os.Exit(1)
+	}
+	if len(args) > 2 {
+		fmt.Printf("too many arguments provided\n")
+		os.Exit(1)
+	}
+	fmt.Printf("starting crawl of: %s\n", args[1])
+	raw_HTML, err := getHTML(args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", raw_HTML)
+}
+
+// getHTML fetches the HTML from rawURL, ensuring the response is both valid
+func getHTML(rawURL string) (string, error) {
+
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch the webpage: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Check for 4xx or 5xx status codes
+	if resp.StatusCode >= 400 {
+		return "", fmt.Errorf("received an error status code: %d", resp.StatusCode)
+	}
+
+	// Verify the response is text/html
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "text/html") {
+		return "", fmt.Errorf("expected 'text/html' content type but got '%s'", contentType)
+	}
+
+	// Read the response body
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+
+	return string(bodyBytes), nil
 }
